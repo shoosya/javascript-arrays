d8e104397f36a762b3c494535df127df316ca292
Natalie Kay <35006220+shoosya@users.noreply.github.com>
2023-06-28 22:48:01 +0000

    Update 5 files

--- 2.js
+++ 2.js
@@ -6,17 +6,41 @@ Given an array containing the english alphabet, write code that returns an array
 
 */
 
-function getAlphaNumericAlphabet(){
-  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  let alphaNumericAlphabet
+function getAlphaNumericAlphabet() {
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let alphaNumericAlphabet = [];
 
-  // Your code here
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  alphaNumericAlphabet.push(...numbers, ...alphabet, "&");
 
-  return alphaNumericAlphabet
+  return alphaNumericAlphabet;
 }
 
-module.exports = getAlphaNumericAlphabet
-
-/*
-You can run tests for this code to check your answers by running `npm run test-2`
-*/
+module.exports = getAlphaNumericAlphabet;
